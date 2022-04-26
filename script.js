24178178778a32831f7da6ecb1133b02edaeb37d
adrianna-thomas <42939699+adrianna-thomas@users.noreply.github.com>
2022-04-26 10:13:49 -0400

    Add files via upload
    Added email validation in js

--- script.js
+++ script.js
@@ -11,5 +11,17 @@ form.addEventListener('submit', (e) => {
         } else {
             input.parentElement.classList.remove('error');
         }
+        if (input.type == 'email') {
+            if (validateEmail(input.value)) {
+                input.parentElement.classList.remove('error');
+            } else {
+                input.parentElement.classList.add('error');
+            }
+        }
     })
 })
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
